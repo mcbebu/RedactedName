6e81dc39ecab5683bf603dab72b1be0efb3f96df
jweeyh <jweeyh@gmail.com>
2023-02-25 15:54:53 +0800

    Add Location, MinimumSpanningTree and Node

---
+++ src/Location.java
@@ -0,0 +1,12 @@
+public class Location {
+    /*
+    Implement a Location class to contain each buyer's location
+     */
+
+    long latitude;
+    long longitude;
+
+    public Location() {
+
+    }
+}
